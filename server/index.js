a5da98b29c6cb1718a051eb061448f96779a9ec3
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-02-01 20:26:09 +0530

    update env and index.js

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ const server = http.createServer(app);
 // ✅ ATTACH SOCKET.IO
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST"],
         credentials: true
     },
