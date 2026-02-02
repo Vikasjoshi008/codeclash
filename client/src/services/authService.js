55e390dffe21dcda2cdd014216e55967fba56336
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-02-02 09:29:30 +0530

    updated socket

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,33 +1,3 @@
-// const API_URL = import.meta.env.VITE_API_URL;
-
-// export const signup = async (userData) => {
-//   const res = await fetch(`${API_URL}/register`, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(userData),
-//   });
-
-//   if (!res.ok) {
-//     throw new Error("Signup failed");
-//   }
-
-//   return res.json();
-// };
-
-// export const login = async (userData) => {
-//   const res = await fetch(`${API_URL}/login`, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(userData),
-//   });
-
-//   if (!res.ok) {
-//     throw new Error("Login failed");
-//   }
-
-//   return res.json();
-// };
-
 import api from "./api";
 
 export const signup = (userData) =>
