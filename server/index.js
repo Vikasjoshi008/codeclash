61f96788dfa9d51b41dcf1013cc0d74dd18f0ae4
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-02-17 23:51:36 +0530

    fix index.js middleware

--- server/index.js
+++ server/index.js
@@ -41,25 +41,6 @@ app.use(
   })
 );
 
-
-// VERY IMPORTANT
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://codeclash-three.vercel.app"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
-
-
 app.use(express.json());
 
 /* ---------- ROUTES ---------- */
@@ -83,7 +64,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173/", 
+      "http://localhost:5173", 
       "https://codeclash-three.vercel.app"
     ],
     methods: ["GET", "POST"],
