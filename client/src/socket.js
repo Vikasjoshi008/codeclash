8b45dd8f1a71806c951ba09aa2c9996900feb9f2
Vikas Joshi <vikasjoshi10889@gmail.com>
2026-02-17 23:32:42 +0530

    update in socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,6 @@ import { io } from "socket.io-client";
 const socket = io(
   import.meta.env.VITE_SOCKET_URL,
   {
-    autoConnect: false,
     transports: ["websocket"],
     withCredentials: true,
   }
